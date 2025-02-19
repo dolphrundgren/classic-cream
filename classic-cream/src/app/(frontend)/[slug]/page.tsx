f3ffb0d27354c026ecf2eb62e61e22c2156d30c3
dolphrundgren <huntertempleman@gmail.com>
2025-02-19 11:13:58 -0500

    Stopped Typescript from yelling at me.

--- classic-cream/src/app/(frontend)/[slug]/page.tsx
+++ classic-cream/src/app/(frontend)/[slug]/page.tsx
@@ -7,14 +7,14 @@ interface SvgArrowValue {
   isRight: boolean
 }
 
-const scrollToElement = (id) => {
+const scrollToElement = (id: string) => {
   const element = document.getElementById(id)
   if (element) {
     element.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' })
   }
 }
 
-const SvgArrow = (props) => {
+const SvgArrow = (props: SvgArrowValue) => {
   const rightArrow = 'M 0 0 L 50 25 L 0 50 L 0 0'
   const leftArrow = 'M 50 50 L 0 25 L 50 0 L 50 50'
   const svgPath = props.isRight ? rightArrow : leftArrow
@@ -47,11 +47,42 @@ type Args = {
   }>
 }
 
+const ProductArray = (props: any) => {
+  if (!props.products) {
+    return <div>Empty</div>
+  } else {
+    const productCount = props.products.docs.length
+    const productArray = props.products.docs.map(function (doc: any, index: number) {
+      const isLast = index + 2 > productCount ? true : false
+      return (
+        <div
+          id={doc.title}
+          key={index}
+          onClick={() => console.log('clicked!')}
+          className={`h-[35rem] w-[20rem] snap-center lg:w-[25rem] bg-gray-200 rounded-xl flex flex-col place-items-center`}
+        >
+          <div className="relative h-[30rem] w-[15rem]">
+            <Image
+              className="object-cover"
+              fill
+              alt="Mocha aerosol can"
+              src={doc.canFrontImage.url}
+            />
+          </div>
+          <h3>{doc.title}</h3>
+          <h3>{doc.microDescription}</h3>
+        </div>
+      )
+    })
+    return productArray
+  }
+}
+
 export default function Page({ params: paramsPromise }: Args) {
   const [products, setProducts] = useState(null)
   const [focusedProductIndex, setProductIndex] = useState(0)
   const [loading, setLoading] = useState(true)
-  const [windowWidth, setWindowWidth] = useState(null)
+  const [windowWidth, setWindowWidth] = useState<number | null>(null)
   const url = 'http://localhost:3000/api/products/'
 
   useEffect(() => {
@@ -68,55 +99,24 @@ export default function Page({ params: paramsPromise }: Args) {
       })
 
   if (loading) return <h1>Loading</h1>
-  const productCount = products.docs.length
-  const productArray = products.docs.map(function (doc, index) {
-    let isLast = index + 2 > productCount ? true : false
+  if (windowWidth) {
     return (
-      <div
-        id={doc.title}
-        key={index}
-        onClick={() => console.log('clicked!')}
-        className={`h-[35rem] w-[20rem] snap-center lg:w-[25rem] bg-gray-200 rounded-xl flex flex-col place-items-center`}
-      >
-        <div className="relative h-[30rem] w-[15rem]">
-          <Image
-            className="object-cover"
-            fill
-            alt="Mocha aerosol can"
-            src={doc.canFrontImage.url}
-          />
-        </div>
-        <h3>{doc.title}</h3>
-        <h3>{doc.microDescription}</h3>
-      </div>
-    )
-  })
-
-  const payload = products.docs[0]
-  const mochaURL = products.docs[0].canFrontImage.url
-  const title = payload.title
-  const microDescription = payload.microProductDescription
-  const longProductDescription = payload.longProductDescription
-  const butterFatPercentage = payload.butterFatPercentage
-  const ingredientsRichText = payload.ingredients
-  const nutritionFactImage = payload.nutritionFactImage
-  console.log(windowWidth)
-  return (
-    <article className="bg-white">
-      <div className="flex flex-col relative   w-full justify-around items-center">
-        <div className="absolute left-0">
-          {windowWidth > 400 ? <SvgArrow isRight={false} /> : null}
-        </div>
-        <h2 className="text-2xl lg:text-5xl">Our Classic Cream Line Up</h2>
-        <div className="flex no-scrollbar box-border  overflow-x-scroll justify-around items-center flex-col w-full h-[calc(100vh-50px)] lg:h-[calc(100vh-100px)] pt-16 pb-24">
-          <div className="flex snap-x md:pl-[70rem] lg:pl-[40rem] pl-[90rem] pr-[2rem] gap-8 flex-row">
-            {productArray}
+      <article className="bg-white">
+        <div className="flex flex-col relative   w-full justify-around items-center">
+          <div className="absolute left-0">
+            {windowWidth > 400 ? <SvgArrow isRight={false} /> : null}
+          </div>
+          <h2 className="text-2xl lg:text-5xl">Our Classic Cream Line Up</h2>
+          <div className="flex no-scrollbar box-border  overflow-x-scroll justify-around items-center flex-col w-full h-[calc(100vh-50px)] lg:h-[calc(100vh-100px)] pt-16 pb-24">
+            <div className="flex snap-x md:pl-[70rem] lg:pl-[40rem] pl-[90rem] pr-[2rem] gap-8 flex-row">
+              <ProductArray products={products} />
+            </div>
+          </div>
+          <div className="absolute right-0">
+            {windowWidth > 400 ? <SvgArrow isRight={true} /> : null}
           </div>
         </div>
-        <div className="absolute right-0">
-          {windowWidth > 400 ? <SvgArrow isRight={true} /> : null}
-        </div>
-      </div>
-    </article>
-  )
+      </article>
+    )
+  }
 }
