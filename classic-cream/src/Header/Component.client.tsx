5ecad951f9c09ae233d0bcf93483cf656b4a6485
dolphrundgren <huntertempleman@gmail.com>
2025-02-25 11:26:53 -0500

    Centered logo in header for mobile view

--- classic-cream/src/Header/Component.client.tsx
+++ classic-cream/src/Header/Component.client.tsx
@@ -31,7 +31,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
-      <div className="py-8 flex justify-between">
+      <div className="py-8 flex flex-row lg:justify-start justify-center">
         <Link href="/">
           <Logo loading="eager" priority="high" />
         </Link>
