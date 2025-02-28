1b90f568a6279448e9e661b8e5c9c645a45bf966
dolphrundgren <huntertempleman@gmail.com>
2025-02-28 12:23:12 -0500

    Modified text styles to be more dynamic

--- classic-cream/src/app/(frontend)/[slug]/page.client.tsx
+++ classic-cream/src/app/(frontend)/[slug]/page.client.tsx
@@ -14,37 +14,34 @@ const PageClient = (props: any) => {
   return (
     <>
       <div
-        className="self-center flex flex-col lg:flex-row
-  items-center justify-center xs:h-[30vh] w-full h-[20vh] lg:h-[50vh] lg:w-[85vw] bg-[#f2f2f2] "
+        className="container self-center flex flex-col lg:flex-row
+  items-center justify-center xs:h-[150px] h-[165px] md:h-[250px] lg:h-[350px] xl:h-[400px] 2xl:h-[435px] w-full"
       >
-        <div className="flex flex-row items-center justify-center w-full h-[20vh] xs:h-[30vh] lg:h-[40vh] lg:w-[65vw] bg-[#d9d9d9] relative">
+        <div className="flex flex-row items-center justify-center h-full w-full   bg-[#d9d9d9] relative">
           <div className="flex z-10  flex-row w-full h-full">
             <RichText
-              className="w-3/5 xs:text-sm text-base md:text-3xl
-  3xl:text-8xl bg-gray-500/30 font-bold self-center p-3 "
+              className="flex flex-col w-3/5 lg:h-[200px] bg-gray-500/30 font-bold self-center justify-center text-left p-3 xs:text-sm lg:text-4xl 2xl:text-5xl"
               data={heroRichText}
             />
           </div>
           <Image className="object-cover z-0" fill alt={heroImageAlt} src={heroImage} />
         </div>
       </div>
-      <div
-        className="self-center flex flex-row items-center
-  lg:justify-start justify-center h-[30vh] w-[80vw] lg:h-[45vh]
-  lg:w-[85vw] bg-[#d9d9d9] m-8 lg:mb-5 rounded-xl"
-      >
-        <div
-          className=" invisible  lg:visible flex flex-col -rotate-[35deg] translate-x-16 items-start self-start mt-24
-  lg:w-1/4 w-0 lg:text-xl 2xl:text-2xl 3xl:text-7xl"
-        >
-          <h4>No Lactose?</h4>
-          <h4>No Sugar?</h4>
-          <h4 className="font-bold"> No Problem!</h4>
+      <div className="mt-4 md:mt-8 lg:mt-10 xl:mt-15 container self-center flex flex-col lg:flex-row items-center justify-center xs:h-[150px] h-[165px] md:h-[250px] lg:h-[350px] xl:h-[400px] 2xl:h-[435px] w-full">
+        <div className="flex flex-row items-center lg:justify-start justify-center rounded-3xl h-full w-full   bg-[#d9d9d9] relative">
+          <div
+            className=" invisible  lg:visible flex flex-col -rotate-[35deg] translate-x-16 items-start self-start mt-24
+  w-0 lg:w-1/4 lg:text-xl xl:text-3xl 2xl:text-2xl "
+          >
+            <h4>No Lactose?</h4>
+            <h4>No Sugar?</h4>
+            <h4 className="font-bold"> No Problem!</h4>
+          </div>
+          <RichText
+            className="w-3/4 lg:w-1/2 text-xs md:text-xl lg:text-2xl xl:text-3xl"
+            data={contentRichText}
+          />
         </div>
-        <RichText
-          className="lg:w-1/2 w-3/4 text-sm md:text-xl lg:text-2xl 3xl:text-6xl"
-          data={contentRichText}
-        />
       </div>
     </>
   )
