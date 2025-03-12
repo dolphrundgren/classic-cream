439881c43a9946ae5f78b25948b2f26da445ea4e
dolphrundgren <huntertempleman@gmail.com>
2025-03-12 12:03:20 -0400

    Added hero can for desktop

--- classic-cream/src/app/(frontend)/[slug]/page.client.tsx
+++ classic-cream/src/app/(frontend)/[slug]/page.client.tsx
@@ -15,7 +15,7 @@ const PageClient = (props: any) => {
   const contentRichText = pageData.layout[0].columns[0].richText
   const contentFocus = pageData.layout[1].media.sizes.medium.url
   const dairyBadge = pageData.layout[2].media.url
-  const premiumBadge = pageData.layout[3].media.sizes.medium.url
+  const premiumBadge = pageData.layout[3].media.url
   return (
     <div className="w-full h-auto bg-[#cccccc]">
       <div
@@ -24,14 +24,14 @@ const PageClient = (props: any) => {
       ></div>
       <div
         className="mt-4 md:mt-8 lg:mt-10 xl:mt-15 container
-  self-center flex flex-col lg:flex-row items-center justify-center
+  self-center flex flex-col lg:flex-row items-center justify-between
   xs:h-[150px] h-[165px] md:h-[250px] lg:h-[350px] xl:h-[400px]
   2xl:h-[635px] w-full mb-[70px]"
       >
         <div
           className="flex flex-row items-center lg:justify-start
   justify-center rounded-3xl h-full w-full   bg-[#ffffff] relative
-  overflow-y-visible"
+  overflow-y-visible gap-4"
         >
           <div
             className="overflow-y-visible relative -mb-[50px]
@@ -47,10 +47,29 @@ const PageClient = (props: any) => {
           <div className="overflow-y-visible -ml-[200px] -mt-[45px] w-[200px] h-[200px] relative self-start z-30">
             <Image className="object-cover overflow-y-visible" fill alt="badge" src={dairyBadge} />
           </div>
-          <RichText
-            className="w-2/5 text-xs md:text-xl lg:text-2xl xl:text-xl"
-            data={contentRichText}
-          />
+          <div className="flex flex-col w-[500px] h-full">
+            <div
+              className="w-[450px] h-[85px] relative
+  justify-self-start -mt-[32px] mb-[55px]"
+            >
+              <Image
+                className="object-cover"
+                fill
+                alt="Premium
+  Quality"
+                src={premiumBadge}
+              />
+            </div>
+            <RichText
+              className="text-xs md:text-xl lg:text-2xl xl:text-xl"
+              data={contentRichText}
+            />
+          </div>
+          <div className="grow flex flex-row place-content-end">
+            <div className="relative h-[565px] w-[315px] ">
+              <Image className="object-cover" fill alt="Hero Image" src={heroImage} />
+            </div>
+          </div>
         </div>
       </div>
       <div
