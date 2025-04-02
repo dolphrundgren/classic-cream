c161cda5c896d44d472e257a3792425f4ca1334f
dolphrundgren <huntertempleman@gmail.com>
2025-04-02 14:04:00 -0400

    Just letting it happen

--- classic-cream/src/payload.config.ts
+++ classic-cream/src/payload.config.ts
@@ -68,9 +68,7 @@ export default buildConfig({
     },
   }),
   collections: [Pages, Posts, Media, Categories, Users, Products, Subscriptions, Messages],
-  cors: {
-    origins: [getServerSideURL(), 'https://classic-cream.vercel.app'].filter(Boolean),
-  },
+  cors: '*',
   globals: [Header, Footer],
   plugins: [
     ...plugins,
