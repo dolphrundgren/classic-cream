0d8569a06227541a079615180a81b0ced64db9c0
dolphrundgren <huntertempleman@gmail.com>
2025-05-23 15:05:15 -0400

    Lifted logic out of dynamic routing folder

--- classic-cream/src/app/(frontend)/[slug]/forms.client.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import React from 'react'
-
-const FormsClient = (props: any) => {
-  return <button onClick={() => console.log(props.forms)}>Forms</button>
-}
-
-export default FormsClient
--- classic-cream/src/app/(frontend)/page.tsx
+++ classic-cream/src/app/(frontend)/page.tsx
@@ -1,3 +1,30 @@
-import PageTemplate from './[slug]/page'
+import React, { useState, useEffect } from 'react'
+import { getPayload } from 'payload'
+import PageClient from './page.client.tsx'
+import ProductClient from './product.client'
+import config from '@payload-config'
+import StoreLocatorClient from './locator.client'
 
-export default PageTemplate
+type Args = {
+  params: Promise<{
+    slug?: string
+  }>
+}
+
+async function retrieveData() {
+  const payload = await getPayload({ config })
+  const products = await payload.find({ collection: 'products' })
+  const pages = await payload.find({ collection: 'pages' })
+  return { products, pages }
+}
+
+export default async function Page({ params }: Args) {
+  const { products, pages } = await retrieveData()
+  return (
+    <>
+      <PageClient pages={pages} />
+      <ProductClient products={products} />
+      <StoreLocatorClient />
+    </>
+  )
+}
